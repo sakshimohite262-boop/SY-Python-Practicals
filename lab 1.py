bed991bdaa0952ff6c44532feb71a5593fd26a1c
sakshimohite262-boop <sakshimohite262@gmail.com>
2026-08-04 14:07:16 +0530

    Add files via upload

---
+++ lab 1.py
@@ -0,0 +1,16 @@
+print("______STUDENT DETAILS______")
+
+Name=input("Enter your Name:")
+Age=input("Enter your Age:")
+City=input("Enter your city:")
+PRN=input("Enter your PRN:")
+ClgName=input("Enter your ClgName:")
+
+
+print("******SHOW STUDENT DETAILS******")
+
+print("Student Name:",Name)
+print("Student Age:",Age)
+print("Student City:",City)
+print("Student PRN:",PRN)
+print("Student ClgName:",ClgName)

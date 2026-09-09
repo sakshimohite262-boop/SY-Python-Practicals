c0097a2c8d3a700ad237cfa7867a6ccf8c844d21
sakshimohite262-boop <sakshimohite262@gmail.com>
2026-09-09 16:02:32 +0530

    Add files via upload

---
+++ exe5(1).py
@@ -0,0 +1,8 @@
+print("******RIGHT ANGLED TRIANGLE*******")
+
+n = int(input("Enter number of rows: "))
+
+for i in range(1, n + 1):
+    for j in range(i):
+        print(i, end=" ")
+    print()
---
+++ exe5(2).py
@@ -0,0 +1,9 @@
+print("***********INVOICE FRAME BORDER***********")
+
+rows = 5
+columns = 30
+
+for i in range(rows):
+    for j in range(columns):
+        print("*", end="")
+    print()

ff2b5c8f0cc8a6f3a8d4d7a96150e1a57af105fb
sakshimohite262-boop <sakshimohite262@gmail.com>
2026-09-09 16:17:54 +0530

    Add files via upload

---
+++ exe8(2).py
@@ -0,0 +1,11 @@
+print("<============== TEXT MODERATION FILTER ===================>")
+
+feedback = input("Enter your feedback: ")
+
+target_words = ["bad", "stupid", "hate"]
+
+for word in target_words:
+    feedback = feedback.replace(word, "****")
+
+print("\nModerated Feedback:")
+print(feedback)
